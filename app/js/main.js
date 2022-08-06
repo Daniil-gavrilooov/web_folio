dfe7b3341a4b99e3bac23e5cb28565ee6225ae9b
Daniil-gavrilooov <daniil..gavrilooov@gmail.com>
2022-08-06 15:56:34 +0300

    finish section (testimonials)

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+
+$(function () {
+    $('.testimonials__slider').slick({
+        slidesToShow: 2,
+        nextArrow: '<img class="next__arrow" src="images/arrow-next_slider.svg" alt="">',
+        prevArrow: ' <img class="prew__arrow" src="images/prew__arrow-slider.svg" alt="">'
+
+    });
+})
