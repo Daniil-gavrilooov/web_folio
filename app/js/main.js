5267a062a87f5a97ae49a04682baf3a10339bc08
Daniil-gavrilooov <daniil..gavrilooov@gmail.com>
2022-08-07 19:37:11 +0300

    adaptiv for section (testimonails)

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,8 @@ $(function () {
         slidesToShow: 2,
         nextArrow: '<img class="next__arrow" src="images/arrow-next_slider.svg" alt="">',
         prevArrow: ' <img class="prew__arrow" src="images/prew__arrow-slider.svg" alt="">'
-
+        
+        
     });
 })
 
