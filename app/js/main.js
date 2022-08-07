8ba386fce3428a946f8f6715d9bc5d129e906da0
Daniil-gavrilooov <daniil..gavrilooov@gmail.com>
2022-08-07 14:30:55 +0300

    adaptiv for section (header)

--- app/js/main.js
+++ app/js/main.js
@@ -7,3 +7,16 @@ $(function () {
 
     });
 })
+
+let burgerBtn = document.querySelector('.header__burger-btn')
+
+burgerBtn.addEventListener(onclick = function(){
+    let burgerMenu = document.querySelector('.header__burger-menu')
+    let headerBasket = document.querySelector('.header__basket')
+    let body = document.querySelector('.body')
+
+    body.classList.toggle("body-lock")
+    burgerBtn.classList.toggle('header__burger-btn--active')
+    burgerMenu.classList.toggle('header__burger-menu--active')
+    headerBasket.classList.toggle('header__basket--active')
+})
